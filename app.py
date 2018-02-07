947d9fc81ab04a11fe75fbc7c6326ae572246f30
rsurya96 <surya.cse_b2014@bsauniv.ac.in>
2018-02-07 15:21:21 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def makeWebhookResult(req):
                 "source": "cust_plan"
                }
     
-    elif req.get("result").get("action") == "Complaint_status.Complaint_status-selectnumber-2.Complaint_status-selectnumber-2-custom":
+    elif req.get("result").get("action") == "Complaint_status.Complaint_status-custom":
         result = req.get("result")
         parameters = result.get("parameters")
         zone = parameters.get("Users")
@@ -70,32 +70,10 @@ def makeWebhookResult(req):
                 "displayText": speech,
                 #"data": {},
                 #"contextOut": [],
-                "source": "Complaint_status - select.number-2"
+                "source": "Complaint_status.Complaint_status-custom"
               }
     
-    elif req.get("result").get("action") == "Complaint_status.Complaint_status-selectnumber.Complaint_status-selectnumber-custom":
-        result = req.get("result")
-        parameters = result.get("parameters")
-        zone = parameters.get("Complaint_id")
-        cusid = {'CW91810' :'C0001', 'CW91811' :'C0002', 'CW91812' :'C0003', 'CW91813' :'C0004', 'CW91814' :'C0005', 'CW91815' :'C0006', 'CW91816' :'C0007', 'CW91817' :'C0008', 'CW91818' :'C0009', 'CW91819' :'C0010', 'CW91820' :'C0011', 'CW91821' :'C0012', 'CW91822' :'C0013', 'CW91823' :'C0014'}
-        name = {'C0001':'John', 'C0002':'Patterson', 'C0003':'Tim', 'C0004':'Merl', 'C0005':'Robert', 'C0006':'Andrea', 'C0007':'Lucy', 'C0008':'Nicole', 'C0009':'Breanna', 'C0010':'Daniel', 'C0011':'Mason', 'C0012':'Sasha', 'C0013':'Baylee', 'C0014':'Gabrial'}
-        ctype = {'CW91810' :'Technical', 'CW91811' :'Billing', 'CW91812' :'Plan', 'CW91813' :'Technical', 'CW91814' :'Account', 'CW91815' :'Billing', 'CW91816' :'Technical', 'CW91817' :'Technical', 'CW91818' :'Account', 'CW91819' :'Plan', 'CW91820' :'Plan', 'CW91821' :'Plan', 'CW91822' :'Technical', 'CW91823' :'Plan'  }
-        ccomm = {'CW91810' :'No signal', 'CW91811' :'Suspend Billing till May 2018', 'CW91812' :'Change plan to Cricket Infinity', 'CW91813' :'Slow Data Speeds', 'CW91814' :'Account Terminated', 'CW91815' :'Resume Cricket Music Service', 'CW91816' :'Data Services not working', 'CW91817' :'Data Services not working', 'CW91818' :'Unable to access account', 'CW91819' :'Enroll for Cricket Student plan', 'CW91820' :'Enroll for Cricket Family plan', 'CW91821' :'Change plan to Cricket Plus plan', 'CW91822' :'No signal', 'CW91823' :'Change to Cricket Basic plan'  }
-        cstat = {'CW91810' :'Resolved', 'CW91811' :'In process', 'CW91812' :'Open', 'CW91813' :'Open', 'CW91814' :'In process', 'CW91815' :'open', 'CW91816' :'Resolved', 'CW91817' :'Resolved', 'CW91818' :'Resolved', 'CW91819' :'Open', 'CW91820' :'In Process', 'CW91821' :'Resolved', 'CW91822' :'Resolved', 'CW91823' :'Resolved'}
-        #complaint_id = zone
-        complaint_type = str(ctype[zone])
-        complaint_comment = str(ccomm[zone])
-        complaint_status = str(cstat[zone])
-        speech = "Hi " + str(name[zone]) + "( " + str(cusid[zone]) + " ) Complaint id: " + zone + " \n Complaint Type: " + complaint_type + " \n Complaint Comment: " + complaint_comment + " \n Current Status: " + complaint_status
-        print("Response:")
-        print(speech)
-        return{
-                "speech": speech,
-                "displayText": speech,
-                #"data": {},
-                #"contextOut": [],
-                "source": "Complaint_status - select.number-1"
-              }
+    
         
     
     else:
