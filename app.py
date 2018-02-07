662f93b80626f806322f86c7152a92198c59b014
rsurya96 <surya.cse_b2014@bsauniv.ac.in>
2018-02-07 10:36:48 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -28,15 +28,16 @@ def webhook():
     return r
 
 def makeWebhookResult(req):
-    if req.get("result").get("action") != "cust_plan.cust_plan-custom":
+    if req.get("result").get("action") == "cust_plan.cust_plan-custom":
         return {}
     result = req.get("result")
     parameters = result.get("parameters")
     zone = parameters.get("Users")
 
-    cost = {'C0001':'Cricket Basic Validity 12-12-18', 'C0002':'Cricket Infinity Validity 1-6-18', 'C0003':'Cricket Student Validity 5-2-18', 'C0004':'Cricket Family Validity 6-6-18', 'C0005':'Cricket Basic Validity 28-2-18', 'C0006':'Cricket Infinity Validity 5-5-18', 'C0007':'Cricket Plus Vality 31-12-18', 'C0008':'Cricket Plus Validity 12-10-18', 'C0009':'Cricket Student Validity 25-5-18', 'C0010':'Cricket Family Validity 22-8-18', 'C0011':'Cricket Basic validity 22-4-18', 'C0012':'Cricket Plus Validity 22-3-18', 'C0013':'Cricket Infinity Validity 28-6-18'}
-
-    speech = "Hi" + zone + " you are enrolled under" + str(cost[zone])
+    name = {'C0001':'John', 'C0002':'Patterson', 'C0003':'Tim', 'C0004':'Merl', 'C0005':'Robert', 'C0006':'Andrea', 'C0007':'Lucy', 'C0008':'Nicole', 'C0009':'Breanna', 'C0010':'Daniel', 'C0011':'Mason', 'C0012':'Sasha', 'C0013':'Baylee', 'C0014':'Gabrial'}
+    cost = {'C0001':'Cricket Basic', 'C0002':'Cricket Infinity', 'C0003':'Cricket Student ', 'C0004':'Cricket Family', 'C0005':'Cricket Basic', 'C0006':'Cricket Infinity', 'C0007':'Cricket Plus', 'C0008':'Cricket Plus', 'C0009':'Cricket Student', 'C0010':'Cricket Family', 'C0011':'Cricket Basic', 'C0012':'Cricket Plus', 'C0013':'Cricket Infinity', 'C0014':'Cricket Student'}
+    validity = {'C0001':'Validity 12-12-18', 'C0002':'Validity 1-6-18', 'C0003':'Validity 5-2-18', 'C0004':'Validity 6-6-18', 'C0005':'Validity 28-2-18', 'C0006':'Validity 5-5-18', 'C0007':'Vality 31-12-18', 'C0008':'Validity 12-10-18', 'C0009':'Validity 25-5-18', 'C0010':'Validity 22-8-18', 'C0011':'validity 22-4-18', 'C0012':'Validity 22-3-18', 'C0013':'Validity 28-6-18',  'C0014':'Validity 4-4-18'}
+    speech = "Hi " + str(name[zone]) +" (" + zone + " ) you are enrolled under " + str(cost[zone] + " with " + str(validity[zone]))
     print("Response:")
     print(speech)
     return {
